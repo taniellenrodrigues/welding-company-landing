c6301b4f9cb31780f7a2d306eb5603e2696d45b6
Taniellen Rodrigues <tanyellens@gmail.com>
2026-05-30 12:32:38 -0300

    rolagem do site com a permanencia do topo e menu de navegação fixos  no topo da página.

--- landing.js
+++ landing.js
@@ -1,7 +1,15 @@
 const burguer = document.querySelector('.burguer');
 const navList = document.querySelector('.nav-list');
+const topo = document.querySelector('.topo');
 
 burguer.addEventListener('click', () => {
     navList.classList.toggle('active');
 
 })
+ window.addEventListener('scroll', () => {
+    if (window.scrollY > 20){
+        topo.classList.add('scrolled');
+    } else{
+        topo.classList.remove(scrolled);
+    }
+ });
