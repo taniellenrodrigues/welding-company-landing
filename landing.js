3c484563251cd4484423301a99447d8925fe1013
Taniellen Rodrigues <tanyellens@gmail.com>
2026-06-06 12:21:38 -0300

    adicionada animação de scroll reveal nos elementos da página

--- landing.js
+++ landing.js
@@ -1,6 +1,19 @@
 const burguer = document.querySelector('.burguer');
 const navList = document.querySelector('.nav-list');
 const topo = document.querySelector('.topo');
+const items = document.querySelectorAll('.reveal')
+
+function mostrarNaTela() {
+    const alturaTela = window.innerHeight;
+    items.forEach(item => {
+        const posicao = item.getBoundingClientRect() .top;
+        
+        if (posicao < alturaTela - 100) {
+            item.classList.add('active');
+        }
+    });
+}
+window.addEventListener('scroll', mostrarNaTela);
 
 burguer.addEventListener('click', () => {
     navList.classList.toggle('active');
